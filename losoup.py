1430f245a04d5aab4ecddcc56fde983fa1e32163
Michał Sidor (Michcioperz) <michcioperz@gmail.com>
2016-04-07 08:42:02 +0200

    I've had lots of fun

--- losoup.py
+++ losoup.py
@@ -1,5 +1,6 @@
 import datetime
 import urllib.parse
+from operator import itemgetter
 
 import requests
 from bs4 import BeautifulSoup
@@ -19,20 +20,25 @@ def image_if_any(x) -> str:
 def get_news(page: int = 1) -> list:
     r = requests.get("http://lo01.pl/staszic/index.php", params=dict(page=page))
     r.encoding = "UTF-8"
-    return [dict(img=image_if_any(x), title=x.find("div", "news_title").get_text(),
-                 id=urllib.parse.parse_qs(urllib.parse.urlparse(x.find("div", "news_title").a["href"]).query)["id"][0],
-                 content=x.find("div", "news_content"),
-                 author=x.find("div", "news_author").get_text().split("dodany przez: ", 1)[1],
-                 time=datetime.datetime.strptime(x.find("div", "news_time").get_text(), "%H:%M %d.%m.%Y")) for x in
-            BeautifulSoup(r.text).find_all("div",
-                                           "news")]
+    n = [dict(img=image_if_any(x), title=x.find("div", "news_title").get_text(),
+              id=urllib.parse.parse_qs(urllib.parse.urlparse(x.find("div", "news_title").a["href"]).query)["id"][0],
+              content=x.find("div", "news_content"),
+              author=x.find("div", "news_author").get_text().split("dodany przez: ", 1)[1],
+              time=datetime.datetime.strptime(x.find("div", "news_time").get_text(), "%H:%M %d.%m.%Y"), pinned=False)
+         for x in BeautifulSoup(r.text).find_all("div", "news")]
+    if page == 1:
+        i = 0
+        while sorted(n[i:], key=itemgetter("time"), reverse=True) != n[i:]:
+            print(i)
+            n[i]["pinned"] = True
+            i += 1
+    return n
 
 
 def get_article(item: int) -> dict:
     r = requests.get("http://lo01.pl/staszic/index.php", params=dict(subpage="news", id=item))
     r.encoding = "UTF-8"
     x = BeautifulSoup(r.text).find("div", "news")
-    print(x)
     return dict(img=image_if_any(x), title=x.find("div", "news_title").get_text(), id=item,
                 content=x.find("div", "news_content"),
                 author=x.find("div", "news_author").get_text().split("dodany przez: ", 1)[1],
@@ -43,7 +49,7 @@ def get_article(item: int) -> dict:
 @app.route('/', defaults={"page": 1})
 @app.route('/p/<int:page>')
 def news_page(page: int) -> Response:
-    return render_template("news_list.html", news=get_news(page), max=max)
+    return render_template("news_list.html", news=get_news(page), max=max, len=len)
 
 
 @app.route('/n/<int:item>')
