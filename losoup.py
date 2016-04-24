a6a2471f4b1f74571d60273fd9ff001e46609515
Michał Sidor (Michcioperz) <michcioperz@gmail.com>
2016-04-24 15:12:24 +0200

    maybe some bleaching will help it all

--- losoup.py
+++ losoup.py
@@ -1,14 +1,14 @@
 import datetime
-import time
-import urllib.parse
 import pickle
+import urllib.parse
 from operator import itemgetter
 
+import bleach
 import requests
 from bs4 import BeautifulSoup
 from flask import Flask, render_template, request, url_for, Response, jsonify, abort
-from werkzeug.contrib.atom import AtomFeed
 from redis import Redis
+from werkzeug.contrib.atom import AtomFeed
 
 app = Flask(__name__)
 cache = Redis()
@@ -40,6 +40,8 @@ def _get_news(page: int = 1) -> list:
             print(i)
             n[i]["pinned"] = True
             i += 1
+    for news in n:
+        news["content"] = bleach.clean(news["content"], strip=True, tags=["p", "strong", "em", "ul", "ol", "li", "img"])
     with cache.pipeline() as pipe:
         pipe.set('p:%i' % page, pickle.dumps(n))
         pipe.expire('p:%i' % page, page_ttl)
@@ -63,6 +65,7 @@ def _get_article(item: int) -> dict:
              author=str(x.find("div", "news_author").get_text()).split("dodany przez: ", 1)[1],
              time=datetime.datetime.strptime(x.find("div", "news_time").get_text(), "%H:%M %d.%m.%Y"),
              cleantext=str(x.find("div", "news_content").get_text()).strip())
+    a["content"] = bleach.clean(a["content"], strip=True, tags=["p", "strong", "em", "ul", "ol", "li", "img"])
     with cache.pipeline() as pipe:
         pipe.set('n:%i' % item, pickle.dumps(a))
         pipe.expire('n:%i' % item, article_ttl)
